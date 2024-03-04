7a4c26420844d166d926ff47c3afe151b21a3776
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-04 10:11:17 -0300

    update overlay

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const overlay = document.getElementById('overlayId');
-
+var html = document.getElementsByTagName('html')[0];
 
 function overlayClick() {
-
+    
+    html.style.position = 'fixed';
     overlay.style.display = 'block';
-
+    
 }
+
+
+
+
