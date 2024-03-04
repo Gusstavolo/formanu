c55c557dbdfd59245e5629af65b3def04156e1af
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-04 11:49:55 -0300

    fixed pos overlay

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ var html = document.getElementsByTagName('html')[0];
 function overlayClick() {
     
     html.style.position = 'fixed';
-    overlay.style.display = 'block';
-    
+    overlay.style.display = 'flex';
+    setTimeout(function() {
+        overlay.style.opacity = '1'; // Define a opacidade do overlay para 1 após um pequeno intervalo para iniciar a transição
+    }, 300); // 10ms de atraso para garantir que a transição seja aplicada
 }
+
 function overlayHeartClick() {
-
+    
     html.style.position = '';
-    overlay.style.display = 'none';
-
-
+    overlay.style.opacity = '0'; // Define a opacidade do overlay para 0 para iniciar a transição de ocultação
+    setTimeout(function() {
+        overlay.style.display = 'none'; // Oculta o overlay após a transição
+    }, 300); // Tempo correspondente à duração da transição definida no CSS (300ms)
 }
-
 
 
